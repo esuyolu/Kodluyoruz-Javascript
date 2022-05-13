8bd799edcb89034367fe58be5ab41944e3466d77
TCESUYOLU <esma.suyolu@turkcell.com.tr>
2022-05-13 14:38:46 +0300

    practices

---
+++ js/practice3.js
@@ -0,0 +1,35 @@
+let userFormDOM = document.querySelector('#userForm')
+userFormDOM.addEventListener('submit', formHandler)
+const alertDOM = document.querySelector('#alert')
+
+const alertFunction = (title, message, className = 'warning') => `
+<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+    <strong>${title}</strong>${message}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+`
+
+function formHandler(event) {
+    event.preventDefault()
+    const USER_NAME = document.querySelector('#username')
+    const SCORE = document.querySelector('#score')
+
+    if (USER_NAME.value && SCORE.value) {
+        addItem(USER_NAME.value, SCORE.value)
+        USER_NAME.value = ""
+        SCORE.value = ""
+    } else {
+        alertDOM.innerHTML = alertFunction('Hata!!!', 'Eksik Bilgi Girdiniz', 'danger')
+    }
+}
+
+let userListDOM = document.querySelector('#userList')
+
+const addItem = (username, score) => {
+    let liDOM = document.createElement('li')
+    liDOM.innerHTML = `${username}  <span class="badge bg-primary rounded-pill"> ${score}</span>`
+    liDOM.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
+    userListDOM.append(liDOM)
+}
